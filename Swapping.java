a9116ca8e5f637f062457c4bf7c80f8fa7d0bdf5
UEM <sreejitriki@gmail.com>
2025-03-20 15:16:19 +0530

    Another Program

--- Swapping.java
+++ Swapping.java
@@ -2,17 +2,8 @@ package typesofdatatypes;
 
 public class Swapping {
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int temp;
-		System.out.println("Before Swapping");
-		System.out.println("a = "+a+" b = "+b);
-		
-		temp = a;
-		a = b;
-		b = temp;
-	
-		System.out.println("After Swapping");
-		System.out.println("a = "+a+" b = "+b);
+		for (int i = 0; i < 10; i++) {
+		System.out.println(i);
+		}
 	}
 }
