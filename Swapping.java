551717b24f822b691fd538d66a0af77e82d0b2ac
UEM <sreejitriki@gmail.com>
2025-03-20 14:36:12 +0530

    changes done

--- Swapping.java
+++ Swapping.java
@@ -4,12 +4,13 @@ public class Swapping {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20;
+		int temp;
 		System.out.println("Before Swapping");
 		System.out.println("a = "+a+" b = "+b);
 		
-		a = a + b;
-		b = a - b;
-		a = a - b;
+		temp = a;
+		a = b;
+		b = temp;
 	
 		System.out.println("After Swapping");
 		System.out.println("a = "+a+" b = "+b);
